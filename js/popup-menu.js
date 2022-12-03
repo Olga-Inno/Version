c87e681eab0ed32e9e4e61cc5b58c445372fd62d
Olga Pavlova Gencha <117361275+Olga-Inno@users.noreply.github.com>
2022-12-04 00:39:33 +0300

    ad

---
+++ js/popup-menu.js
@@ -0,0 +1,6 @@
+'use strict';
+const menuWrapper = document.querySelector('.popup-menu');
+const menuButton = document.querySelector('.popup-menu__button');
+const menulist = document.querySelector('.popup-menu__list');
+
+document.addEventListener ('click',closeMenu);
